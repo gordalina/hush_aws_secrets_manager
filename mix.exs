3a652aeaa3e377f537ffd94dfdcd4dd01a57c770
Samuel Gordalina <samuel.gordalina@gmail.com>
2022-08-23 12:27:08 -1000

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -16,7 +16,13 @@ defmodule HushAwsSecretsManager.MixProject do
       start_permanent: Mix.env() == :prod,
       source_url: @source_url,
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.github": :test],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.github": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ],
       dialyzer: [
         plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
       ]
@@ -33,12 +39,13 @@ defmodule HushAwsSecretsManager.MixProject do
     [
       {:ex_aws, "~> 2.0"},
       {:ex_aws_secretsmanager, "~> 2.0"},
-      {:hush, "~> 0.4"},
+      {:hackney, "~> 1.17"},
+      {:hush, path: "../hush"},
       {:mox, "~> 0.5", only: :test},
-      {:ex_check, "~> 0.12.0", only: :dev, runtime: false},
-      {:credo, "~> 1.4.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
-      {:sobelow, "~> 0.10.3", only: :dev, runtime: false},
+      {:ex_check, "~> 0.12", only: :dev, runtime: false},
+      {:credo, "~> 1.4", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:sobelow, "~> 0.10", only: :dev, runtime: false},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
       {:excoveralls, "~> 0.12", only: :test}
     ]
